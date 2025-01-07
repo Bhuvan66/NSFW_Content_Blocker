4369353af53e917517cee12864afd410a3227c85
Bhuvan R Shetty <110040586+Bhuvan66@users.noreply.github.com>
2025-01-07 16:13:27 +0530

    Only swear words are censored
    created a list that has swear words to censor in tweets

--- Extention/content.js
+++ Extention/content.js
@@ -3,6 +3,45 @@
   async function extractTweets() {
 
     let originalTweets = [];
+    swearWordList = [
+      '2g1c', 'Asshole', 'Bastard', 'Bitch', 'Bloody', 'Blowjob', 'Bullshit',
+      'Clusterfuck', 'Cock', 'Cocksucker', 'Cunt', 'Dick', 'Faggot', 'Fuck',
+      'Fuckery', 'Healslut', 'Jesus Fuckin Christ', 'Kike', 'Motherfucker',
+      'Nigga', 'Nigger', 'Poofter', 'Prick', 'Pussy', 'Ratfucking', 'Retard',
+      'Shit', 'Shithouse', 'Shitposting', 'Shitter', 'Slut', 'Son of a bitch',
+      'Twat', 'Unclefucker', 'Wanker', 'Whore', 'acrotomophilia', 'anal',
+      'anilingus', 'anus', 'apeshit', 'arsehole', 'ass', 'asshole', 'assmunch',
+      'babeland', 'ball', 'bang', 'bangbros', 'bangbus', 'bareback', 'barenaked',
+      'bastard', 'bastardo', 'bastinado', 'batter', 'bbw', 'bdsm', 'beaner',
+      'beaners', 'beastiality', 'bestiality', 'big cock', 'bimbos', 'birdlock',
+      'bitch', 'bitches', 'blowjob', 'blumpkin', 'bollocks', 'bondage', 'boner',
+      'boob', 'boobs', 'booty', 'breasts', 'bukkake', 'busty', 'butt', 'buttcheeks',
+      'butthole', 'camgirl', 'camslut', 'camwhore', 'chick', 'cleaver', 'cleveland',
+      'clit', 'clitoris', 'clusterfuck', 'cock', 'cocks', 'cum', 'coon', 'coons',
+      'coprolagnia', 'coprophilia', 'cornhole', 'cowgirl', 'creampie', 'cum',
+      'cumming', 'cumshot', 'cumshots', 'cunnilingus', 'cunt', 'cup', 'damn',
+      'darkie', 'date', 'daterape', 'deepthroat', 'dendrophilia', 'dick', 'dildo',
+      'dingleberries', 'dingleberry', 'doggie', 'doggiestyle', 'doggy', 'doggystyle',
+      'dp', 'double penetration', 'ecchi', 'ejaculation', 'erotic', 'erotism',
+      'eunuch', 'faggot', 'femdom', 'fetish', 'figging', 'fingerbang', 'fingering',
+      'fisting', 'footjob', 'frotting', 'fuck', 'fuckin', 'fucking', 'fucktards',
+      'fudge', 'fudgepacker', 'futanari', 'g-spot', 'gag', 'gang', 'gangbang',
+      'gay', 'goregasm', 'grope', 'handjob', 'hardcore', 'hentai', 'hole', 'homoerotic',
+      'honkey', 'hooker', 'horny', 'humping', 'incest', 'intercourse', 'jack', 'jerk',
+      'kinkster', 'kinky', 'knobbing', 'knockers', 'livesex', 'masturbate', 'masturbating',
+      'masturbation', 'milf', 'missionary', 'motherfucker', 'murder', 'negro', 'nig',
+      'nigga', 'nigger', 'nympho', 'nymphomania', 'octopussy', 'orgasm', 'orgy',
+      'paedophile', 'panties', 'panty', 'pedophile', 'pegging', 'penetration', 'penis',
+      'pissing', 'pisspig', 'porn', 'porno', 'pornography', 'pussy', 'queaf', 'queef',
+      'quim', 'raghead', 'raging', 'rape', 'raping', 'rapist', 'rimjob', 'rimming',
+      'scat', 'schlong', 'scissoring', 'semen', 'sex', 'sexcam', 'sexo', 'sexual',
+      'sexuality', 'sexually', 'sexy', 'shaved', 'shemale', 'shibari', 'shit', 'shitblimp',
+      'shitty', 'shota', 'sisterfucker', 'slut', 'smut', 'snowballing', 'squirting',
+      'strapon', 'suck', 'sucking', 'suicide', 'sultry', 'swinger', 'threesome', 'throat',
+      'throating', 'tit', 'tits', 'titties', 'titty', 'tubgirl', 'tushy', 'twat', 'twink',
+      'vagina', 'venus', 'viagra', 'vibrator', 'wank', 'wet', 'wetback', 'whore', 'worldsex',
+      'wtf', 'xx', 'xxx', 'yaoi', 'yiffy', '🖕'
+    ];
 
     setTimeout(async () => {
       // Select all <article> tags (tweets are typically wrapped in them)
@@ -27,17 +66,20 @@
         // Storin og twt text for uncensoring later
         originalTweets.push({ element: tweetTextElement, originalText: tweetText });
 
+        const isTweetNSFW = await checkNSFW(tweetText);
         let words = tweetText.split(/\s+/);
         let censoredText = "";
         let hasCensoredWords = false;
 
         for (let i = 0; i < words.length; i++) {
           const word = words[i];
-          const isNSFW = await checkNSFW(word);
-          if (isNSFW) {
+          const isNSFWWord = swearWordList.includes(word.toLowerCase());
+          if (isTweetNSFW && isNSFWWord) {
             hasCensoredWords = true;
+            censoredText += (i > 0 ? " " : "") + "*".repeat(word.length);
+          } else {
+            censoredText += (i > 0 ? " " : "") + word;
           }
-          censoredText += (i > 0 ? " " : "") + (isNSFW ? "*".repeat(word.length) : word);
         }
 
         tweetTextElement.innerText = censoredText;
@@ -92,25 +134,25 @@
   }
 
   // Function to send words to the server for NSFW classification
-  async function checkNSFW(word) {
+  async function checkNSFW(text) {
     try {
       const response = await fetch("http://127.0.0.1:5000/analyze", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ tweets: [{ text: word }] }),
+        body: JSON.stringify({ tweets: [{ text: text }] }),
       });
 
       const data = await response.json();
       if (data && data[0].label === "nsfw") {
-        return true; // Word is NSFW
+        return true; // Tweet is NSFW
       } else {
-        return false; // Word is safe
+        return false; // Tweet is safe
       }
     } catch (error) {
-      console.error("Error sending word to the server:", error);
-      return false; // In case of error, assume word is safe
+      console.error("Error sending text to the server:", error);
+      return false; // In case of error, assume tweet is safe
     }
   }
 
